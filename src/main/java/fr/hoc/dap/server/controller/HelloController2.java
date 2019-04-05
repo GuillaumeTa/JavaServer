31ff9e4a072cfff46aeba797ef82908c903d0452
alucardozo <guillaume.tatuszka@gmail.com>
2019-04-05 13:20:54 +0200

    Mise en place de thymeLeaf

---
+++ src/main/java/fr/hoc/dap/server/controller/HelloController2.java
@@ -0,0 +1,35 @@
+package fr.hoc.dap.server.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Alucard
+ * Controller de test html
+ */
+@Controller
+public class HelloController2 {
+
+    /**
+     * @return la vue hello.html
+     * @param model spring
+     */
+    @RequestMapping("/hello2")
+    public String hello(final ModelMap model) {
+        model.addAttribute("maVar", "toto");
+
+        List<String> bestioles = new ArrayList();
+
+        bestioles.add("Chien");
+        bestioles.add("Zebre");
+        bestioles.add("Chien");
+
+        model.addAttribute("bebetes", bestioles);
+
+        return "hello";
+    }
+}
